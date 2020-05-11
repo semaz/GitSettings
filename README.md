a72288659c52abf0e5e66c46dbd337d975c9c095
Creel <devcreel@gmail.com>
2020-05-11 21:26:21 +0300

    fix install process

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ Useful GitHub settings - configs and ignore.
 ## Install ##
 1. `git clone https://github.com/DevCreel/GitSettings.git ~/GitSettings`
 2. Copy file `names.sh.dist` to `names.sh` and set your name and email.
-3. Run `sh ~/GitSettings/install.sh`.
+3. Run `bash ~/GitSettings/install.sh`.
 4. Reload bash.
--- install.sh
+++ install.sh
@@ -1,12 +1,9 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-if [ -f "~/.gitconfig" ]; then
-    rm ~/.gitconfig
-fi
+SCRIPTS_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-GITSETTINGS_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-ln -s $GITSETTINGS_PATH/gitconfig ~/.gitconfig
+rm -f ~/.gitconfig
+ln -s $SCRIPTS_PATH/gitconfig ~/.gitconfig
 
 get_rc_path() {
   if [ -f ~/.bash_profile ]
@@ -24,11 +21,19 @@ get_rc_path() {
   fi
 }
 
-bashinst() {
-    if ! grep -q "$1" ~/$(get_rc_path); then
-        echo "\n# $2 \n$1" >> ~/$(get_rc_path)
-    fi;
-}
+if grep -q "GITSETTINGS_PATH" ~/$(get_rc_path);
+then
+    echo "Scripts already installed."
+    exit 0;
+fi;
 
-bashinst "export GITSETTINGS_PATH=$GITSETTINGS_PATH" "Git Settings Path";
-bashinst "source $GITSETTINGS_PATH/data.sh" "Git Settings Data";
+tee -a ~/$(get_rc_path) >/dev/null <<EOF
+
+# Git Settings Path
+export GITSETTINGS_PATH=$SCRIPTS_PATH
+
+# Git Settings Data
+source $SCRIPTS_PATH/data.sh
+EOF
+
+exec $SHELL -l
