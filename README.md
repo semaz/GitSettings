240ec49b24fa5629a0cd4d5702fa3f3bdb4f18d4
semaz <devcreel@gmail.com>
2023-06-25 17:36:46 +0300

    fix install script for zsh

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 Useful GitHub settings - configs and ignore.
 
 ## Install ##
-1. `git clone https://github.com/DevCreel/GitSettings.git ~/GitSettings`
+1. `git clone git@github.com:semaz/GitSettings.git ~/GitSettings`
 2. Copy file `names.sh.dist` to `names.sh` and set your name and email.
 3. Run `bash ~/GitSettings/install.sh`.
 4. Reload bash.
--- install.sh
+++ install.sh
@@ -1,36 +1,55 @@
 #!/bin/bash
 
-SCRIPTS_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SCRIPTS_PATH="$( cd "$( dirname "${BASH_SOURCE[0]:-${(%):-%x}}" )" && pwd )"
 
 rm -f ~/.gitconfig
 ln -s $SCRIPTS_PATH/gitconfig ~/.gitconfig
 
-get_rc_path() {
-  if [ -f ~/.bash_aliases ]
-  then
-     echo ".bash_aliases";
-  elif [ -f ~/.bash_profile ]
-  then
-     echo ".bash_profile";
-  elif [ -f ~/.bash_rc ]
-  then
-     echo ".bash_rc";
-  elif [ -f ~/.bashrc ]
-  then
-     echo ".bashrc";
-  else
-     echo "Can't find bach config file";
-     exit 1;
-  fi
+get_config_path() {
+  case $SHELL in
+  */zsh)
+     if [ -f ~/.zprofile ]
+        then
+           echo ".zprofile";
+        elif [ -f ~/.zshrc ]
+        then
+           echo ".zshrc";
+        else
+           echo "Can't find zsh config file";
+           exit 1;
+        fi
+     ;;
+  */bash)
+     if [ -f ~/.bash_aliases ]
+        then
+           echo ".bash_aliases";
+        elif [ -f ~/.bash_profile ]
+        then
+           echo ".bash_profile";
+        elif [ -f ~/.bash_rc ]
+        then
+           echo ".bash_rc";
+        elif [ -f ~/.bashrc ]
+        then
+           echo ".bashrc";
+        else
+           echo "Can't find bash config file";
+           exit 1;
+        fi
+     ;;
+  *)
+    echo "Unknown shell";
+    exit 1;
+  esac
 }
 
-if grep -q "GITSETTINGS_PATH" ~/$(get_rc_path);
+if grep -q "GITSETTINGS_PATH" ~/$(get_config_path);
 then
     echo "Scripts already installed."
     exit 0;
 fi;
 
-tee -a ~/$(get_rc_path) >/dev/null <<EOF
+tee -a ~/$(get_config_path) >/dev/null <<EOF
 
 # Git Settings
 export GITSETTINGS_PATH=$SCRIPTS_PATH
